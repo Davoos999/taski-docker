53a1e2e63f01df712e585b5c36cab142f8280c73
Davoos999 <dsosipov999@gmail.com>
2025-12-25 10:19:05 +0300

    2342

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -23,7 +23,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders',
+    'corsheaders.middleware.CorsMiddleware',
     'rest_framework',
 ]
 
